06e7509a709905f1b8087a377c02b3b1bb3d24d4
Soukhin Das <sdas@Soukhins-MacBook-Pro.local>
2020-09-09 19:34:19 -0400

    Working vgg, high acc'

--- code/test.py
+++ code/test.py
@@ -17,6 +17,7 @@ import math
 model = VGG16(weights='imagenet',
               include_top=False)
 #%%
+'''
 batch_size = 32
 datagen = ImageDataGenerator(rescale = 1./255,  
           zoom_range = 0.1,
@@ -24,7 +25,7 @@ datagen = ImageDataGenerator(rescale = 1./255,
           height_shift_range = 0.2,
           horizontal_flip = True,
           fill_mode ='nearest')
-train_it = datagen.flow_from_directory('stim/',
+train_it = datagen.flow_from_directory('train/',
                                        batch_size = batch_size, 
                                        target_size = (224, 224),
                                        class_mode = 'categorical',
@@ -51,7 +52,44 @@ nb_test_samples = len(test_it.filenames)
 
 predict_size_train = int(math.ceil(nb_train_samples / batch_size)) 
 predict_size_val = int(math.ceil(nb_val_samples / batch_size)) 
-predict_size_test = int(math.ceil(nb_test_samples / batch_size)) 
+predict_size_test = int(math.ceil(nb_test_samples / batch_size))'''
+#%%
+
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.vgg16 import preprocess_input
+
+def convertimgs(path,data) :
+    for dirName, subdir, files in os.walk(path):
+        for filename in sorted(files):
+            if filename == '.DS_Store':
+                continue
+            ds = load_img(path +'/' + filename,target_size = (224,224))
+            im = img_to_array(ds)
+            #im = im.reshape((1, im.shape[0], im.shape[1], im.shape[2]))
+            im = preprocess_input(im)
+            data.append(im) 
+    return data
+
+
+train_it = []
+train_it = convertimgs('data/train/faces',train_it)
+train_it = convertimgs('data/train/Scenes',train_it)
+train_it = convertimgs('data/train/objects',train_it)
+
+val_it = []
+val_it = convertimgs('data/val/faces',val_it)
+val_it = convertimgs('data/val/scenes',val_it)
+val_it = convertimgs('data/val/objects',val_it)
+
+test_it = []
+test_it = convertimgs('data/test/faces',test_it)
+test_it = convertimgs('data/test/scenes',test_it)
+test_it = convertimgs('data/test/objects',test_it)
+
+train_it = np.array(train_it)
+val_it = np.array(val_it)
+test_it = np.array(test_it)
+print(train_it.shape,val_it.shape,test_it.shape)
 #%%
 start = time.time()
 features_train = model.predict(train_it) 
@@ -70,43 +108,6 @@ np.save('features_train' , features_train)
 np.save('features_val', features_val)
 np.save('features_test', features_test)
 #%%
-from keras.preprocessing.image import load_img, img_to_array
-from keras.applications.vgg16 import preprocess_input
-
-def convertimgs(path,data) :
-    for dirName, subdir, files in os.walk(path):
-        for filename in sorted(files):
-            if filename == '.DS_Store':
-                continue
-            ds = load_img(path +'/' + filename,target_size = (224,224))
-            im = img_to_array(ds)
-            #im = im.reshape((1, im.shape[0], im.shape[1], im.shape[2]))
-            im = preprocess_input(im)
-            data.append(im) 
-    return data
-
-
-train_it = []
-train_it = convertimgs('stim/faces',train_it)
-train_it = convertimgs('stim/Scenes',train_it)
-train_it = convertimgs('stim/objects',train_it)
-
-val_it = []
-val_it = convertimgs('val/faces',val_it)
-val_it = convertimgs('val/scenes',val_it)
-val_it = convertimgs('val/objects',val_it)
-
-test_it = []
-test_it = convertimgs('test/faces',test_it)
-test_it = convertimgs('test/scenes',test_it)
-test_it = convertimgs('test/objects',test_it)
-
-train_it = np.array(train_it)
-val_it = np.array(val_it)
-test_it = np.array(test_it)
-
-
-#%%
 from keras.layers import Flatten,Dense,Dropout
 from keras.models import Sequential
 from keras.utils.np_utils import to_categorical
@@ -116,18 +117,18 @@ epochs = 30
 
 train_data = np.load('features_train.npy')
 #train_data = features_train
-train_labels = [0] * 400 + [1]*400 + [2] * 400
+train_labels = [0] * 500 + [1]*500 + [2] * 500
 train_labels = to_categorical(train_labels, 3)
 
 
 val_data = np.load('features_val.npy')
 #val_data = features_val
-val_labels = [0] * 40 + [1]*40 + [2] * 40
+val_labels = [0] * 50 + [1]*50 + [2] * 50
 val_labels = to_categorical(val_labels, 3)
 
 test_data = np.load('features_test.npy')
 #test_data = features_test
-test_labels = [0] * 40 + [1]*40 + [2] * 40
+test_labels = [0] * 50 + [1]*50 + [2] * 50
 test_labels = to_categorical(test_labels, 3)
 
 
@@ -168,7 +169,7 @@ print(metric)
 import itertools
 import pandas as pd
 from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as pltzz
+import matplotlib.pyplot as plt
 categorical_test_labels = pd.DataFrame(test_labels).idxmax(axis=1)
 categorical_preds = pd.DataFrame(pred).idxmax(axis=1)
 confusion_matrix= confusion_matrix(categorical_test_labels, categorical_preds)
