22b1cd39127d395b95c3ccff2596aa246b144a6a
Soukhin Das <sdas@Soukhins-MacBook-Pro.local>
2020-09-22 23:19:12 -0400

    Added tuning calculations

--- tools/tun_calc.py
+++ tools/tun_calc.py
@@ -7,15 +7,14 @@ Created on Tue Sep 22 01:36:27 2020
 """
 import numpy as np
 
-
-def calc_avgtun(train_it,activation_model,layer_names,):
-    tun_value = np.zeros((len(layer_names),600))
-    for img_tensor in train_it:
+def calc_tun(data,activation_model,layer_names):
+    tun_value = np.zeros((len(data),len(layer_names),600))
+    for imgs,img_tensor in enumerate(data):
       img_tensor = img_tensor.reshape([1,224,224,3])
       intermediate_activations = activation_model.predict(img_tensor)
       for l in range(len(layer_names)):
         layer_activation = intermediate_activations[l]
         for k in range(layer_activation.shape[3]):
-          tun_value[l,k] += np.mean(layer_activation[0,:,:,k])
+          tun_value[imgs,l,k] += np.mean(layer_activation[0,:,:,k])
           
     return tun_value
